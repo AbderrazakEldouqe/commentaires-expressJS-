e42259fa721ec375da974dc47190cb4e48ba3749
eldouqe <abderrazak.eldouqe1@gmail.com>
2020-05-30 21:10:54 +0200

    Add body-parser

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-let express = require('express'); 
+let express = require('express');
 let app = express();
 
-app.set('view engine','ejs');
+let bodyParser = require('body-parser');
 
-app.use('/assets',express.static('public'));
+//Moteur de templates
+app.set('view engine', 'ejs');
 
+// Middleware
+app.use('/assets', express.static('public'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+//Routes
 app.get('/', (request, response) => {
-    response.render('pages/index',{test:'salut'});
+    response.render('pages/index', { test: 'salut' });
+});
+
+app.post('/', (request, response) => {
+    console.log(request.body);
 });
 
 app.listen(8080);
